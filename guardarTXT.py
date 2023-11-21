e33ee76e5ec9e6f5a7cf092a510a1029d082f894
RUIZ SANDOVAL KEVIN YAEL <78570278+RSKY18141051@users.noreply.github.com>
2023-11-21 17:32:39 -0600

    merge (#34)
    * Identificador de voz
    
    * deberia de tener un cambio a la voz
    
    * Merge pull request #31 from RSKY18141051/main (#32)
    
    * Reloj merge (#33)
    
    * reloj creado
    
    * Función de recordatorio unido a reloj
    
    #6
    
    * Create Reloj digital
    
    Es el mismo reloj pero e digital
    
    * Rename Reloj digital to Reloj digital.py
    
    * Create Reloj_2.0_T.py
    
    Relog actualizado
    
    * modificacion al codigo para que guarde txt en una carpeta
    
    * agrego algunos archivos para yo usarlos desde mi casa
    
    * video de apoyo para github
    
    ---------
    
    Co-authored-by: joseluna1 <144958830+joseluna1@users.noreply.github.com>
    Co-authored-by: FidelXXl <102196459+FidelXXl@users.noreply.github.com>
    Co-authored-by: Emanuel Lopez <l18141057@queretaro.tecnm.mx>
    
    ---------
    
    Co-authored-by: FidelXXl <102196459+FidelXXl@users.noreply.github.com>
    Co-authored-by: joseluna1 <144958830+joseluna1@users.noreply.github.com>
    Co-authored-by: Emanuel Lopez <l18141057@queretaro.tecnm.mx>

---
+++ guardarTXT.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+import time
+import math
+
+def update_clock():
+    # Resto del código para actualizar el reloj...
+    
+    # Llamar a esta función nuevamente después de 1000 ms (1 segundo)
+    root.after(1000, update_clock)
+
+def display_reminders():
+    reminders = [
+        "Recordatorio 1: Reunión a las 10:00 AM",
+        "Recordatorio 2: Llamada con el cliente a las 3:00 PM",
+        "Recordatorio 3: Completar informe antes de las 5:00 PM"
+    ]
+
+    # Guardar la lista de recordatorios en un archivo .txt
+    with open('recordatorios.txt', 'w') as file:
+        for reminder in reminders:
+            file.write(reminder + '\n')
+
+    # Limpiar el widget de etiqueta
+    reminder_label.config(text='\n'.join(reminders))
+
+    # Llamar a esta función cada 60000 ms (1 minuto) para actualizar los recordatorios
+    root.after(60000, display_reminders)
+
+root = tk.Tk()
+root.title("Reloj Analógico")
+
+canvas = tk.Canvas(root, width=300, height=300)
+canvas.pack()
+
+# Dibujar el dial del reloj
+canvas.create_oval(50, 50, 250, 250)
+for i in range(60):
+    angle = math.radians(i * 6)
+    if i % 5 == 0:
+        length = 10
+    else:
+        length = 5
+    canvas.create_line(150 + 100 * math.cos(angle) * 0.9, 150 - 100 * math.sin(angle) * 0.9, 150 + 100 * math.cos(angle) * 0.95, 150 - 100 * math.sin(angle) * 0.95, width=2, fill="black")
+    canvas.create_text(150 + 120 * math.cos(angle) * 0.8, 150 - 120 * math.sin(angle) * 0.8, text=str(i + 1), fill="black")
+
+# Crear un widget de etiqueta para mostrar los recordatorios
+reminder_label = tk.Label(root, text="", justify="left")
+reminder_label.pack(pady=10)
+
+# Iniciar la función para mostrar los recordatorios
+display_reminders()
+
+# Iniciar la función para actualizar el reloj
+update_clock()
+
+root.mainloop()
